5f6e3d3cc79c9d9e1129810597c0f30ea43774f3
Megan Dotto <megan.dotto@gmail.com>
2018-03-28 06:27:48 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,10 +1,12 @@
 
 def hopper
 	programmer_hash = {
+		:grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
         }
-
+			}
+		programmer_hash[:grace_hopper]
 
 end
 
