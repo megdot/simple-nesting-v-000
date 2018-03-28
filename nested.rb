f587e4acee9fcde1abd6fbba394740e82ddbe4ef
Megan Dotto <megan.dotto@gmail.com>
2018-03-28 07:02:38 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -74,7 +74,7 @@ def adding_to_dennis
           :languages => "C"
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+		 [:languages] << "Assembly"
 		 programmer_hash
 
 end
