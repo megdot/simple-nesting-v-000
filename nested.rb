e0191ec5b214c8fe45506878252d7c6d0ed3553a
Megan Dotto <megan.dotto@gmail.com>
2018-03-28 06:21:16 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -12,10 +12,7 @@ end
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 
-	programmer_hash =
- 		{
-          :known_for => "Object Orientation"
-        }
+	programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
