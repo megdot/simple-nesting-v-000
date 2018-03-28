cf0270d9232b55d47b9faa9fd0f88eedc8ceb4ab
Megan Dotto <megan.dotto@gmail.com>
2018-03-28 06:58:39 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -69,18 +69,12 @@ end
 def adding_to_dennis
 	programmer_hash =
  		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        },
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
         :dennis_ritchie => {
           :known_for => "Unix",
           :languages => "C"
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages] >> "Assembly"
+		 programmer_hash
 
 end
